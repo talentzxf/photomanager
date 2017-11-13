62fc31c36c31554c53d51ae66f65b38a28b9f268
vincentzhang <vincentzhang@ea.com>
2017-11-13 19:28:00 +0800

    Added face recognition support now

--- PhotoManager/utilities.py
+++ PhotoManager/utilities.py
@@ -1,8 +1,12 @@
+import uuid
+
 import os
+from PIL import Image
 from django.utils import timezone
 
 from PhotoManager.models import ImageFile, Album
 from PhotoManager.settings import DATA_DIR
+import face_recognition
 
 
 class Utilities:
@@ -31,18 +35,53 @@ class Utilities:
         return False
 
     @staticmethod
+    def get_face_infor(local_path, file_name):
+        full_file_path = os.path.join(local_path, file_name)
+        img_to_test = face_recognition.load_image_file(full_file_path)
+        face_locations = face_recognition.face_locations(img_to_test, model="cnn")
+
+        found_faces = len(face_locations)
+        for face_location in face_locations:
+            top, right, bottom, left = face_location
+            print("A face is located at pixel Top:{}, Left:{}, Bottom:{}, Right: {}".format(top, left, bottom, right))
+            face_image = img_to_test[top:bottom, left:right]
+            pil_image = Image.fromarray(face_image)
+            Utilities.save_face_image(pil_image)
+
+        face_encodings = face_recognition.face_encodings(img_to_test, face_locations)
+        encoded_faces = len(face_encodings)
+        # for face_encoding in face_encodings:
+        # print(face_encoding)
+        if encoded_faces != found_faces:
+            print("ERRORERRORERRORERRORERRORERRORERROR")
+
+    @staticmethod
+    def save_face_image(pil_image):
+        face_folder = os.path.join(Utilities._data_dir, "faces")
+        if not os.path.exists(face_folder):
+            os.makedirs(face_folder)
+        face_full_path = os.path.join(face_folder, uuid.uuid4().hex + ".jpg")
+        pil_image.save(face_full_path, "JPEG")
+        return
+
+    @staticmethod
     def scan_data_folder(album):
         scanned_number = 0
-        for root, dir, files in os.walk(Utilities._data_dir):
-            for file in files:
-                if Utilities.scan_image(root, file, album):
+        for root, dir, filenames in os.walk(os.path.join(Utilities._data_dir, "raw_images")):
+            for filename in filenames:
+                print("Registering file:{}".format(filename))
+                if Utilities.scan_image(root, filename, album):
+                    Utilities.get_face_infor(root, filename)
                     scanned_number += 1
+                print("Scanning file:{}".format(filename))
+                print("File:{} done".format(filename))
         return scanned_number
 
     @staticmethod
     def upload_file(file):
-        with open(os.path.join(Utilities._data_dir, file.name), 'wb+') as destination:
+        with open(os.path.join(Utilities._data_dir, "raw_images", file.name), 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
             Utilities.scan_image(Utilities._data_dir, file.name)
+            Utilities.get_face_infor(Utilities._data_dir, file.name)
         return True
